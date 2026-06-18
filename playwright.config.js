c6614410fa27009245b829ac4c3b68ada2916ed9
shrinivas-varex <shrinivas.teradal@vareximaging.com>
2026-06-18 21:12:38 +0530

    playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@playwright/test');
 
-const { defineConfig } = require('@playwright/test');
-
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 30_000,
